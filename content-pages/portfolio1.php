8903f69c97df359d018223ff6c40534a5844e591
altair961 <altair961@users.noreply.github.com>
2018-08-06 23:11:55 +0300

    Added images and styles

---
+++ content-pages/portfolio1.php
@@ -0,0 +1,9 @@
+<div id="step-2" class="group">
+	<h2 id="step-num-lede">Step 2</h2>
+	<h3 id="welcome-txt">Here you can find the portfolio</h3> 
+	<img id="pic-roc-fly" src="../images/descktop/pictures/step-2.png" alt="step-2" class="group" />
+</div>
+<?php
+	include "../Site-light/photo/index.php";
+?>
+<div class="portfolio"></div>
