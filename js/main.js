f9fccb13deed16e6947e67a6e024c7b90b7c12d7
altair961 <altair961@users.noreply.github.com>
2018-08-19 10:33:22 +0300

    Fixed amount of scrolling when the #header element gets hidden.

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ function switchToPage(evt, cityName) {
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
 var currentScrollPos = window.pageYOffset;
-  if (currentScrollPos > 200) {
+  if (currentScrollPos > 300) {
     var header = document.getElementById("header");
     header.style.display = "none";
     document.getElementById("blog").style.width = "100%";
