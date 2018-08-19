b160a44618260b0931dd147b23a947a073091c06
altair961 <altair961@github.com>
2018-08-19 16:12:13 +0200

    Added another button to the tabs.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 //switch between tabs
-function switchToPage(evt, cityName) {
+function switchToPage(evt, contentPage) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tab-content");
     for (i = 0; i < tabcontent.length; i++) {
@@ -9,7 +9,7 @@ function switchToPage(evt, cityName) {
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-    document.getElementById(cityName).style.display = "block";
+    document.getElementById(contentPage).style.display = "block";
     evt.currentTarget.className += " active";
 }
 
